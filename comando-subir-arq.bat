c628fc4a4a06587c92c039ab4e9fd0936b26a555
annabmaker <annabmaker@gmail.com>
2024-08-09 22:20:35 -0300

    aula do dia xxxx

--- comando-subir-arq.bat
+++ comando-subir-arq.bat
@@ -2,7 +2,7 @@ echo OFF
 
 echo --- Comando para subir arquivos ------
 git add .
-git commit -m "alterar mensagem" 
+git commit -m "aula do dia xxxx" 
 git push
 echo --- fim ---
 echo --- Anna Beatriz---
