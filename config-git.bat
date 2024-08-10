b144ad9ad4c70b1ccfa4a0224733525a78fefea9
annabmaker <annabmaker@gmail.com>
2024-08-09 21:41:35 -0300

    Primeira pagina Web

---
+++ config-git.bat
@@ -0,0 +1,12 @@
+echo OFF
+
+echo --- Configurando Usuario GitHub ------
+echo 
+echo 
+git config --global user.name "annabmaker"
+git config --global user.email annabmaker@gmail.com
+git config --list
+echo --- fim ---
+echo --- Anna Beatriz---
+
+pause 
